ed22e8a67e4475d94eafcae767803da6045b2033
tachibana-kei <kissyr119@gmail.com>
2021-01-27 12:08:36 +0000

    課題６を修正

--- kadai-06/index2.js
+++ kadai-06/index2.js
@@ -2,14 +2,30 @@ try {
     const str1 = "abc"
     JSON.parse(str1)
 }catch (error){
-   //
-   //
-   //
-   console.log(error)
+   // 正しいJSONではないのでパースできない。
+   // 文字列だけのJSONはダブルクオーテーションで囲む必要がある。
+   // 正しくは const str1 = '"abc"' のようなJSON文字列になる。
 }
+
 try {
     const str2 = "'abc'"
     JSON.parse(str2)
+} catch (error) {
+    // JSONでの文字列はシングルクオートではなくダブルクオートで囲む必要があるため。
+}
 
     const str3 = '"abc"'
-    JSON.parse(str3
+    JSON.parse(str3)
+
+
+
+    const str5 = '{ "a": 123 }'
+    JSON.parse(str5)
+
+    try {
+        const str6 = '{ a: 123 }'
+        JSON.parse(str6)
+    } catch (error) {
+        console.log(error)
+        //JSONのキーはダブルクオーテーションで囲む必要があるため。
+    }
